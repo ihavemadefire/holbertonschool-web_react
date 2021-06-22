ec709157a9a5c3f7cfdb75b60270068158262558
Jacob <jacobmeide@gmail.com>
2021-06-22 00:17:16 -0500

    add typing to vars

--- 0x00-TypeScript/task_0/js/main.ts
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -19,19 +19,20 @@ const student2: Student = {
     location: "New York",
 };
 
-const studentsList = [ student1, student2];
-const table = document.createElement('table');
+const studentsList: Array<Student> = [ student1, student2];
+const table: HTMLTableElement = document.createElement('table');
 document.body.appendChild(table);
-const head = table.createTHead();
-const row = head.insertRow();
-const th1 = row.insertCell(0);
-const th2 = row.insertCell(1);
+const head: HTMLTableSectionElement = table.createTHead();
+const row: HTMLTableRowElement = head.insertRow();
+const th1: HTMLTableHeaderCellElement = row.insertCell(0);
+const th2: HTMLTableHeaderCellElement = row.insertCell(1);
 th1.innerHTML = ("<b>First Name</b>");
 th2.innerHTML = ("<b>Location</b>");
+const body: HTMLTableSectionElement = table.createTBody();
 studentsList.map((student) => {
-	const newRow = table.insertRow();
-	const firstNameRow = newRow.insertCell();
-	const locationRow = newRow.insertCell();
+	const newRow: HTMLTableRowElement = body.insertRow();
+	const firstNameRow: HTMLTableCellElement = newRow.insertCell();
+	const locationRow: HTMLTableCellElement= newRow.insertCell();
 	firstNameRow.innerHTML = student.firstName;
 	locationRow.innerHTML = student.location;
 })
