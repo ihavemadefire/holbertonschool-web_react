b54853749226bf0311d4cbfb5cfdb8900e4de7a9
Jacob <jacobmeide@gmail.com>
2021-06-22 22:05:23 -0500

    singluar. Plural...whats the diference

--- 0x00-TypeScript/task_5/js/main.ts
+++ 0x00-TypeScript/task_5/js/main.ts
@@ -1,17 +1,17 @@
 export interface MajorCredits {
 	_brand: 'major';
-	credits: number;
+	credit: number;
 }
 
 export interface MinorCredits {
 	_brand: 'minor';
-	credits: number;
+	credit: number;
 }
 
 export function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits) {
-	return { credits: subject1.credits + subject2.credits } as MajorCredits;
+	return { credit: subject1.credit + subject2.credit } as MajorCredits;
 }
 
 export function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits) {
-	return { credits: subject1.credits + subject2.credits } as MinorCredits;
+	return { credit: subject1.credit + subject2.credit } as MinorCredits;
 }
