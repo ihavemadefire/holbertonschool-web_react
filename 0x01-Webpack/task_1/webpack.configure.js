2058d7091a0e0c2cb7402a9579f9eabb39b59d7c
Jacob <jacobmeide@gmail.com>
2021-06-24 22:19:59 -0500

    I hate this language. I hate anything to do with node.

---
+++ 0x01-Webpack/task_1/webpack.configure.js
@@ -0,0 +1,11 @@
+
+const path = require("path");
+
+module.exports = {
+  entry: "./js/dashboard_main.js",
+  mode: "production",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public")
+  }
+};
