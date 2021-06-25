61e21b2ea2d76013b80df3d147a4f25efb46517d
Jacob <jacobmeide@gmail.com>
2021-06-24 23:33:16 -0500

    I really hope to christ this works

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,15 +1,10 @@
-const $ = require( "jquery" );
+const path = require('path');
 
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button>Click here to get started</button>');
-$('body').append('<p id=\'count\'></p>');
-$('body').append('<p>Copyright - Holberton School</p>');
-
-button.addEventListener("click", _.debounce(updateCounter));
-
-function updateCounter() {
-  const clicks = $('#count').html().split(" ", 1) + 1 || 1;
-  $('#count').html(`${clicks} clicks on the button`);
+module.exports = {
+	entry: './js/dashboard_main.js',
+	output: {
+		path: path.resolve(__dirname, 'public'),
+		filename: 'bundle.js'
+	},
+	mode: 'production',
 }
-// "500"
