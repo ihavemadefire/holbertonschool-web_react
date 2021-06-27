10b9f33c01acf6e833616038eec03eb440037ec5
Jacob <jacobmeide@gmail.com>
2021-06-26 21:55:01 -0500

    update dashboard and add webpack

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,10 +1,16 @@
-const path = require('path');
+import $ from "jquery";
+const _ = require('lodash');
 
-module.exports = {
-	entry: './js/dashboard_main.js',
-	output: {
-		path: path.resolve(__dirname, 'public'),
-		filename: 'bundle.js'
-	},
-	mode: 'production',
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+$('button').on('click', _.debounce(updateCounter, 250));
+
+let i = 0;
+function updateCounter() {
+	i += 1;
+	$('#count').html(`${i} clicks on the button`);
 }
