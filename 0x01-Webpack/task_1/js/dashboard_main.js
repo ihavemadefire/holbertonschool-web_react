aa4d003c440e4e5ed387280d500345815636bcc0
Jacob <jacobmeide@gmail.com>
2021-06-26 22:04:40 -0500

    Change wait time

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -7,7 +7,7 @@ $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
-$('button').on('click', _.debounce(updateCounter, 250));
+$('button').on('click', _.debounce(updateCounter, 500));
 
 let i = 0;
 function updateCounter() {
