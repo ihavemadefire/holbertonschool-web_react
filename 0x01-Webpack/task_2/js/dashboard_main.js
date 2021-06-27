81c3e079ec5526a70575a7d5f5a84cc5009726c2
Jacob <jacobmeide@gmail.com>
2021-06-26 23:09:56 -0500

    Initial attempt

---
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,16 @@
+import $ from "jquery";
+const _ = require('lodash');
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+$('button').on('click', _.debounce(updateCounter, 500));
+
+let i = 0;
+function updateCounter() {
+	i += 1;
+	$('#count').html(`${i} clicks on the button`);
+}
