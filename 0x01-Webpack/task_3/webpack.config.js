b0297aa4d0cb2515a6fab5b3e35d4e6e643b933a
Jacob <jacobmeide@gmail.com>
2021-06-27 00:06:07 -0500

    Initial attempt

---
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+	entry: {
+		header: './modules/header/header.js',
+		body: './modules/body/body.js',
+		footer: './modules/footer/footer.js',
+		},
+	  mode: "development",
+		plugins: [
+		new HTMLWebpackPlugin(),
+		new CleanWebpackPlugin()
+	  ],
+	output: {
+		filename: "[name].bundle.js",
+    	path: path.resolve(__dirname, "public")
+	},
+	performance: {
+		maxAssetSize: 100000,
+	  },
+	  devServer: {
+		contentBase: './public',
+		compress: true,
+		port: 8564,
+	  },
+	module: {
+		rules: [
+		  {
+			test: /\.css$/i,
+			use: ["style-loader", "css-loader"],
+		  },
+		  {
+			test: /\.css$/, use: ['style-loader', 'css-loader']
+		},
+		{
+			test: /\.(gif|png|jpe?g|svg)$/i,
+			use: [
+				'file-loader',
+				{
+					loader: 'image-webpack-loader',
+					options: {
+						bypassOnDebug: true, // webpack@1.x
+						disable: true, // webpack@2.x and newer
+					},
+				},
+			],
+		}
+		],
+	  },
+}
