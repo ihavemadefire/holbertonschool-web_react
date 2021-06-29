b9cc4d7c9b42a9544c077d768d8c9aeea15ad79d
Jacob <jacobmeide@gmail.com>
2021-06-28 19:43:59 -0500

    First attempt at number 3

--- 0x02-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x02-react_intro/task_2/dashboard/src/Notifications.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import image from './close-icon.png';
+import { getLatestNotification } from './utils';
 import './Notifications.css'
 
 export function Notifications() {
+  function logOnClick() {
+    console.log("Close button has been clicked");
+  }
+
     return (
       <div className="Notifications">
+      <button aria-label="Close" onClick={logOnClick} style={{position:'absolute', top: 15, right: 10, visibility: "hidden"}}>
+        <img alt="close" src={image} style={{visibility: "visible", height: "15px", width: "15px"}}/>
+      </button>
         <p>
           Here is the list of notifications
         </p>
+        <ul>
+          <li data-priority='default'>New course available</li>
+          <li data-priority='urgent'>New resume available</li>
+          <li data-priority="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification()}} />
+        </ul>
       </div>
     );
   }
