944f78aa34bf825c3f1834ab99e65824a626c4e1
Jacob <jacobmeide@gmail.com>
2021-06-29 20:02:38 -0500

    Attempt task 5

---
+++ 0x02-react_intro/task_3/dashboard/src/App.js
@@ -0,0 +1,33 @@
+import logo from './holbie_logo.jpeg';
+import { getFullYear, getFooterCopy } from './utils';
+import './App.css';
+import {Notifications} from './Notifications'
+
+function App() {
+  return (
+    <div className="App">
+        <div id='root-notifications'>
+          {Notifications()}
+        </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+        <form>
+          <label for="email">Email: </label>
+          <input type="email" id="email"></input>
+          <label for="password">Password: </label>
+          <input type="password" id="password"></input>
+          <button>OK</button>
+        </form>
+      </body>
+      <footer className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
