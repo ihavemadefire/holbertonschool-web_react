a3cb4922f92d8c0db64d827388efd63226b080bd
Jacob <jacobmeide@gmail.com>
2021-06-30 21:52:02 -0500

    initial attempt at number 8

---
+++ 0x02-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+    },
+    mode: 'development',
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          // type: 'asset/resource',
+          use: [
+            'file-loader',
+            {
+              loader: 'image-webpack-loader',
+              options: {
+                bypassOnDebug: true, // webpack@1.x
+                disable: true, // webpack@2.x and newer
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx'],
+    },
+    devServer: {
+      // contentBase: './dist',
+      compress: true,
+      hot: true,
+      // port: 8564,
+    },
+    devtool: 'inline-source-map',
+    plugins: [new HtmlWebpackPlugin({ template: './dist/index.html' })],
+  }
+  
